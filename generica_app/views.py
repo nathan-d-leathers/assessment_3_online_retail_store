26e7d5850d35b712809b7cd85eac08a972a5658c
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-07-08 08:44:37 -0700

    working on cart function

--- generica_app/views.py
+++ generica_app/views.py
@@ -9,134 +9,133 @@ import os
 import pprint
 load_dotenv()
 
-electronics_list = [
-    {
-        'id': 1,
-        'title': 'Computer',
-        'description': 'Surf the Net with Ease!',
-        'price': '1800.00',
-        'img_url': 'static/assets/laptop.jpeg'
-    },
-    {
-        'id': 2,
-        'title': 'Video Game Console',
-        'description': 'Adventure Awaits',
-        'price': '600.00',
-        'img_url': 'static/assets/xbox.jpeg'
-    },
-    {
-        'id': 3,
-        'title': 'Television',
-        'description': 'Stream your favorite shows right to your home',
-        'price': '800.00',
-        'img_url': 'static/assets/tv.jpeg'
-    },
-    {
-        'id': 4,
-        'title': 'Stereo',
-        'description': 'Get the party started',
-        'price': '400.00',
-        'img_url': 'static/assets/stereo.jpeg'
-    },
-]
-
-outdoors_list = [
-    {
-        'id': 1,
-        'title': 'Tent',
-        'description': 'Spend a night sleeping under the stars',
-        'price': '300.00',
-        'img_url': 'static/assets/tent.jpeg'
-    },
-    {
-        'id': 2,
-        'title': 'Grill',
-        'description': 'Charcaol not included',
-        'price': '450.00',
-        'img_url': 'static/assets/grill.webp'
-    },
-    {
-        'id': 3,
-        'title': 'Canoe',
-        'description': 'Check out a new kind of Streaming',
-        'price': '750.00',
-        'img_url': 'static/assets/canoe.jpeg'
-    },
-    {
-        'id': 4,
-        'title': 'Bicycle',
-        'description': 'Eco-friendly Transportation powered by YOU',
-        'price': '1200.00',
-        'img_url': 'static/assets/bicycle.jpeg'
-    },
-]
-
-bed_and_bath_list = [
-    {
-        'id': 1,
-        'title': 'Bed',
-        'description': 'Spend a third of your life in comfort',
-        'price': '2400.00',
-        'img_url': 'static/assets/bed.jpeg'
-    },
-    {
-        'id': 2,
-        'title': 'Toilet',
-        'description': 'A porcelin throne fit for a king',
-        'price': '150.00',
-        'img_url': 'static/assets/toilet.jpeg'
-    },
-    {
-        'id': 3,
-        'title': 'Dresser',
-        'description': 'Wardrobe storage at an affordable price',
-        'price': '280.00',
-        'img_url': 'static/assets/dresser.jpeg'
-    },
-    {
-        'id': 4,
-        'title': 'Shower',
-        'description': 'Wake up feeling refreshed',
-        'price': '120.00',
-        'img_url': 'static/assets/shower.webp'
-    },
-]
-
-kitchen_list = [
-    {
-        'id': 1,
-        'title': 'Stove',
-        'description': 'The heart of the kitchen',
-        'price': '750.00',
-        'img_url': 'static/assets/stove.jpeg'
-    },
-    {
-        'id': 2,
-        'title': 'Refridgerator',
-        'description': 'Keep your food cold and fresh',
-        'price': '900.00',
-        'img_url': 'static/assets/fridge.jpeg'
-    },
-    {
-        'id': 3,
-        'title': 'Pots and Pans',
-        'description': 'Somethings about to smell great!',
-        'price': '220.00',
-        'img_url': 'static/assets/pots.jpeg'
-    },
-    {
-        'id': 4,
-        'title': 'Dishwasher',
-        'description': 'Making cleaning a breeze',
-        'price': '440.00',
-        'img_url': 'static/assets/dishwasher.jpeg'
-    },
+product_list = [
+    [
+        {
+            'id': 1,
+            'title': 'Computer',
+            'description': 'Surf the Net with Ease!',
+            'price': '1800.00',
+            'img_url': 'static/assets/laptop.jpeg'
+        },
+        {
+            'id': 2,
+            'title': 'Video Game Console',
+            'description': 'Adventure Awaits',
+            'price': '600.00',
+            'img_url': 'static/assets/xbox.jpeg'
+        },
+        {
+            'id': 3,
+            'title': 'Television',
+            'description': 'Stream your favorite shows right to your home',
+            'price': '800.00',
+            'img_url': 'static/assets/tv.jpeg'
+        },
+        {
+            'id': 4,
+            'title': 'Stereo',
+            'description': 'Get the party started',
+            'price': '400.00',
+            'img_url': 'static/assets/stereo.jpeg'
+        },
+    ],
+    [
+        {
+            'id': 5,
+            'title': 'Tent',
+            'description': 'Spend a night sleeping under the stars',
+            'price': '300.00',
+            'img_url': 'static/assets/tent.jpeg'
+        },
+        {
+            'id': 6,
+            'title': 'Grill',
+            'description': 'Charcoal not included',
+            'price': '450.00',
+            'img_url': 'static/assets/grill.webp'
+        },
+        {
+            'id': 7,
+            'title': 'Canoe',
+            'description': 'Check out a new kind of Streaming',
+            'price': '750.00',
+            'img_url': 'static/assets/canoe.jpeg'
+        },
+        {
+            'id': 8,
+            'title': 'Bicycle',
+            'description': 'Eco-friendly Transportation powered by YOU',
+            'price': '1200.00',
+            'img_url': 'static/assets/bicycle.jpeg'
+        },
+    ],
+    [
+        {
+            'id': 9,
+            'title': 'Bed',
+            'description': 'Spend a third of your life in comfort',
+            'price': '2400.00',
+            'img_url': 'static/assets/bed.jpeg'
+        },
+        {
+            'id': 10,
+            'title': 'Toilet',
+            'description': 'A porcelain throne fit for a king',
+            'price': '150.00',
+            'img_url': 'static/assets/toilet.jpeg'
+        },
+        {
+            'id': 11,
+            'title': 'Dresser',
+            'description': 'Wardrobe storage at an affordable price',
+            'price': '280.00',
+            'img_url': 'static/assets/dresser.jpeg'
+        },
+        {
+            'id': 12,
+            'title': 'Shower',
+            'description': 'Wake up feeling refreshed',
+            'price': '120.00',
+            'img_url': 'static/assets/shower.webp'
+        },
+    ],
+    [
+        {
+            'id': 13,
+            'title': 'Stove',
+            'description': 'The heart of the kitchen',
+            'price': '750.00',
+            'img_url': 'static/assets/stove.jpeg'
+        },
+        {
+            'id': 14,
+            'title': 'Refrigerator',
+            'description': 'Keep your food cold and fresh',
+            'price': '900.00',
+            'img_url': 'static/assets/fridge.jpeg'
+        },
+        {
+            'id': 15,
+            'title': 'Pots and Pans',
+            'description': 'Somethings about to smell great!',
+            'price': '220.00',
+            'img_url': 'static/assets/pots.jpeg'
+        },
+        {
+            'id': 16,
+            'title': 'Dishwasher',
+            'description': 'Making cleaning a breeze',
+            'price': '440.00',
+            'img_url': 'static/assets/dishwasher.jpeg'
+        },
+    ]
 ]
 
 # Create your views here.
 
 # Demo that cart will populate from this list
-# Issue I am tryign to solve:How do I retrieve data from catagory lists when button is submitted and append that data into this list
+# Issue I am trying to solve:How do I retrieve data from category lists when button is submitted and append that data into this list
 
 cart_list = [
     '1',
@@ -166,24 +165,28 @@ def products(request):
 
 
 def kitchen(request):
+    kitchen_list = product_list[3]
     data = {'kitchen_list': kitchen_list}
     response = render(request, 'generica_app/kitchen.html', data)
     return response
 
 
 def bed_and_bath(request):
+    bed_and_bath_list = product_list[2]
     data = {'bed_and_bath_list': bed_and_bath_list}
     response = render(request, 'generica_app/bed_and_bath.html', data)
     return response
 
 
 def electronics(request):
+    electronics_list = product_list[0]
     data = {'electronics_list': electronics_list}
     response = render(request, 'generica_app/electronics.html', data)
     return response
 
 
 def outdoors(request):
+    outdoors_list = product_list[1]
     data = {'outdoors_list': outdoors_list}
     response = render(request, 'generica_app/outdoors.html', data)
     return response
@@ -200,7 +203,7 @@ def searchpage(request):
     return response
 
 
-# not working cart function 
+# not working cart function
 
 # def add_to_cart(request):
 #     new_cart_item = {
